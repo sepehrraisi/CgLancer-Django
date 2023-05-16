bb91df173d970b9a942141ddca633476b4dce5a2
Sepehr Raisi <sepehr.raisi@gmail.com>
2023-05-16 15:32:43 +0330

    Fix: Login Page

--- account_module/views.py
+++ account_module/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 from django.views import View
 
 from account_module.forms import LoginForm, RegisterForm, UserCreateForm, UserEditForm
-from account_module.mixins import JustSuperUser
+# from account_module.mixins import JustSuperUser
 from account_module.models import *
 
 
@@ -82,85 +82,85 @@ class LogoutView(LoginRequiredMixin, View):
         return redirect(reverse('login_view'))
 
 
-class UserListView(JustSuperUser, View):
-    def get(self, request):
-        context = {
-            'users': User.objects.all().order_by("-is_superuser")
-        }
-        return render(request, 'account_module/user-list.html', context)
-
-
-class UserCreateView(JustSuperUser, View):
-    def get(self, request):
-        form = UserCreateForm()
-        context = {
-            "form": form,
-        }
-        return render(request, "account_module/user-create.html", context)
-
-    def post(self, request):
-        form = UserCreateForm(request.POST, request.FILES)
-        if form.is_valid():
-            email = form.cleaned_data.pop("email")
-            password = form.cleaned_data.pop("password")
-            is_superuser = form.cleaned_data.get("is_superuser")
-            try:
-                if is_superuser:
-
-                    new_user = User.objects.create_superuser(email, password, **form.cleaned_data)
-                else:
-                    new_user = User.objects.create_user(email, password, **form.cleaned_data)
-                return redirect(reverse("user_list_view"))
-            except IntegrityError as e:
-                form.add_error("email", "ایمیل و یا کد ملی وارد شده تکراری میباشد")
-                form.add_error("national_code", "ایمیل و یا کد ملی وارد شده تکراری میباشد")
-        context = {
-            'form': form
-        }
-        return render(request, "account_module/user-create.html", context)
-
-
-class UserDeleteView(JustSuperUser, View):
-    http_method_names = ["get"]
-
-    def get(self, request, pk):
-        try:
-            current_user = User.objects.get(id=pk)
-        except User.DoesNotExist:
-            return JsonResponse({"status": "error", "message": "کاربر مورد نظر یافت نشد"})
-        if current_user.is_superuser:
-            return JsonResponse({"status": "error", "message": "نمیتوانید کاربر با دسترسی ویژه را حذف کنید"})
-        current_user.delete()
-        return JsonResponse({"status": "success", "message": "کاربر با موفقیت حذف شد"})
-
-
-class UserEditView(JustSuperUser, View):
-    def get(self, request, pk):
-        user: User = get_object_or_404(User, pk=pk, is_superuser=False)
-        # user_init = {
-        #     "full_name": user.full_name,
-        #     "email": user.email,
-        #     "national_code": user.national_code,
-        #     "avatar": user.avatar,
-        #     "birthday_date": user.birthday_date,
-        # }
-        form = UserEditForm(instance=user)
-        context = {
-            "user": user,
-            "form": form,
-        }
-        return render(request, "account_module/user-edit.html", context)
-
-    def post(self, request, pk):
-        user = get_object_or_404(User, pk=pk, is_superuser=False)
-        form = UserEditForm(request.POST, request.FILES, instance=user)
-        print("valid")
-        if form.is_valid():
-            form.save()
-            return redirect(reverse("user_list_view"))
-        context = {
-            "user": user,
-            "form": form,
-        }
-        return render(request, "account_module/user-edit.html", context)
-
+# class UserListView(JustSuperUser, View):
+#     def get(self, request):
+#         context = {
+#             'users': User.objects.all().order_by("-is_superuser")
+#         }
+#         return render(request, 'account_module/user-list.html', context)
+#
+#
+# class UserCreateView(JustSuperUser, View):
+#     def get(self, request):
+#         form = UserCreateForm()
+#         context = {
+#             "form": form,
+#         }
+#         return render(request, "account_module/user-create.html", context)
+#
+#     def post(self, request):
+#         form = UserCreateForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             email = form.cleaned_data.pop("email")
+#             password = form.cleaned_data.pop("password")
+#             is_superuser = form.cleaned_data.get("is_superuser")
+#             try:
+#                 if is_superuser:
+#
+#                     new_user = User.objects.create_superuser(email, password, **form.cleaned_data)
+#                 else:
+#                     new_user = User.objects.create_user(email, password, **form.cleaned_data)
+#                 return redirect(reverse("user_list_view"))
+#             except IntegrityError as e:
+#                 form.add_error("email", "ایمیل و یا کد ملی وارد شده تکراری میباشد")
+#                 form.add_error("national_code", "ایمیل و یا کد ملی وارد شده تکراری میباشد")
+#         context = {
+#             'form': form
+#         }
+#         return render(request, "account_module/user-create.html", context)
+#
+#
+# class UserDeleteView(JustSuperUser, View):
+#     http_method_names = ["get"]
+#
+#     def get(self, request, pk):
+#         try:
+#             current_user = User.objects.get(id=pk)
+#         except User.DoesNotExist:
+#             return JsonResponse({"status": "error", "message": "کاربر مورد نظر یافت نشد"})
+#         if current_user.is_superuser:
+#             return JsonResponse({"status": "error", "message": "نمیتوانید کاربر با دسترسی ویژه را حذف کنید"})
+#         current_user.delete()
+#         return JsonResponse({"status": "success", "message": "کاربر با موفقیت حذف شد"})
+#
+#
+# class UserEditView(JustSuperUser, View):
+#     def get(self, request, pk):
+#         user: User = get_object_or_404(User, pk=pk, is_superuser=False)
+#         # user_init = {
+#         #     "full_name": user.full_name,
+#         #     "email": user.email,
+#         #     "national_code": user.national_code,
+#         #     "avatar": user.avatar,
+#         #     "birthday_date": user.birthday_date,
+#         # }
+#         form = UserEditForm(instance=user)
+#         context = {
+#             "user": user,
+#             "form": form,
+#         }
+#         return render(request, "account_module/user-edit.html", context)
+#
+#     def post(self, request, pk):
+#         user = get_object_or_404(User, pk=pk, is_superuser=False)
+#         form = UserEditForm(request.POST, request.FILES, instance=user)
+#         print("valid")
+#         if form.is_valid():
+#             form.save()
+#             return redirect(reverse("user_list_view"))
+#         context = {
+#             "user": user,
+#             "form": form,
+#         }
+#         return render(request, "account_module/user-edit.html", context)
+#
