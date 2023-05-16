2211653d56753ed321b948e985b8f4ebd0a93439
Sepehr Raisi <sepehr.raisi@gmail.com>
2023-05-16 20:39:07 +0330

    Update: Added Categories to home page

--- main_module/views.py
+++ main_module/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View
-
+from products_module.models import Category
 
 # Create your views here.
 class HomeView(View):
     def get(self, request):
-        return render(request, "main_module/home.html")
+        context = {
+            'categories': Category.objects.all()
+        }
+        return render(request, "main_module/home.html", context)
