b127be4b7c76e358ab9d0c60b6c5bb37afb6b3bd
Sepehr Raisi <sepehr.raisi@gmail.com>
2023-05-16 10:20:37 +0330

    Fix: Account_module

--- CgLancerDjango/settings.py
+++ CgLancerDjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account_module',
 ]
 
 MIDDLEWARE = [
@@ -117,8 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/medias/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'account_module.User'
+LOGIN_URL = "accounts/login/"
