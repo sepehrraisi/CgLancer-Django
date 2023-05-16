21cea41f25919893a1d7e4e0ec0f35af5e4777af
Sepehr Raisi <sepehr.raisi@gmail.com>
2023-05-16 20:21:17 +0330

    Add: Products & Category Pages

--- products_module/views.py
+++ products_module/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import View
+from products_module.models import Products, Category
+
 
 # Create your views here.
+class ProductsListView(View):
+    def get(self, request):
+        context = {
+            'products': Products.objects.all()
+        }
+        return render(request, 'products_module/products-list.html', context)
+
+
+class CategoryListView(View):
+    def get(self, request):
+        context = {
+            'categories': Category.objects.all()
+        }
+        return render(request, 'products_module/category-list.html', context)
