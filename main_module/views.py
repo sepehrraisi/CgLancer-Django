647c1a80af95933712990b8820be62dd5d2d480b
Sepehr Raisi <sepehr.raisi@gmail.com>
2023-05-17 00:16:44 +0330

    Update: Sliders.

--- main_module/views.py
+++ main_module/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views import View
-from products_module.models import Category
+from products_module.models import Category, Products
 
 # Create your views here.
 class HomeView(View):
     def get(self, request):
         context = {
-            'categories': Category.objects.all()
+            'categories': Category.objects.all(),
+            'products': Products.objects.all()
         }
         return render(request, "main_module/home.html", context)
